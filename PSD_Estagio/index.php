00c6c279b079333df225fbfc6b946b936c9c5641
jobsonjose <jjortb@gmail.com>
2017-06-15 15:54:33 -0300

    Add files via upload

---
+++ PSD_Estagio/index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PDS Projeto</title>
+    <link rel="stylesheet" type="text/css" href="style/Estilo.css">
+    <script type="text/javascript" src="jq/jquery-3.2.1.js"></script>
+    <link rel="stylesheet" type="text/css" href="style/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+        <div class="jumbotron text-center" id="home">
+        <nav class="navbar">
+            <div id="barrasuper">
+            <div class="row">
+            <button id="logar">Logo</button>
+            <button class="btn btn-link" id="increment"><i class="fa fa-handshake-o"> Conheça</i></button>
+            <button class="btn btn-link" id="increment"><i class="fa fa-window-restore"> Vantagens</i></button>
+            <button class="btn btn-link" id="increment"><i class="fa fa-comment"> Fale Conosco</i></button>
+            <button class="btn btn-link" id="increment"><i class="fa fa-phone">  55 (81) 99718-9464</i></button>
+            </div>
+            </div>
+        </nav>
+            <h2>Better Lorem Ipsum</h2>
+            <p>Projeto de Otimização de empresas</p>
+            <p id="obrigatorio">Campos com " * " são obrigatorios</p>
+
+            <form id="pesquisa" action="#" method="POST" accept-charset="utf-8">
+            <label><input type="orb" name="Convidado" placeholder="Convidado Identifique-se"></label>
+            <label><input type="text" name="pesquisa" placeholder="*Pesquisa"></label>
+            <label><input type="text" name="relentes" placeholder="*Empresas Otimizadas"></label>
+            <label><input type="orb" name="desenvolvimento" placeholder="Empresas Desenvolvimentos"></label>
+            <label><input type="orb" name="diretores" placeholder="Diretores de Projetos"></label>
+            <input type="button" class="btn btn-info" name="enviar" value="Buscar">
+            </form>
+        </div>
+
+        <div class="super">
+        <div class="sub">
+            <i class="fa fa-hospital-o fa-5x"></i>
+            <h4>Gestão Medical</h4>
+            <p>Desenvolvidos para ser uma ferramenta de apoio de processos e decisões....</p>
+
+        </div>
+        <div class="sub">
+            <i class="fa fa-search fa-5x"></i>
+            <h4>Monitoramento</h4>
+            <p>A inteligência de gestão de processos que aprendemos nos varios anos....</p>
+        </div>
+        <div class="sub">
+            <i class="fa fa-id-card-o fa-5x"></i>
+            <h4>Integração</h4>
+            <p>Os processos administrativos e médicos são unicos e precisam de uma visão</p>
+        </div>
+        <div class="sub">
+            <i class="fa fa-address-book-o fa-5x"></i>
+            <h4>Usabilidade</h4>
+            <p>Precisa ser Facil de usar. Não queremos profissionais de sistemas</p>
+        </div>
+        </div>
+
+        <div id="desenvolvimento">
+            <div id="center">
+            <h3>Nosso Diferenciais</h3>
+                <h4>Competitivos</h4>
+                <p><strong>Speed up the line with quick, easy modfires</strong> Light soy, extra noodles item modifiers make it simples and
+                efficient to get your customer's orders just right</p>
+            </div>
+            <div class="container">
+                <div id="azul">
+                    <h3>Pensando para ser Amigavel</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div id="preto">
+                    <h3>Eliminar Tarefas Repetitivas</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+
+            </div>
+        </div>
+
+
+
+
+</body>
+</html>
+<script>
+    $("#logar").click(function(){
+
+    })
+</script>
